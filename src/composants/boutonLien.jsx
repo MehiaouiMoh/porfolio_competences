c4484ac418b316cb52931b91ec9d3bdff188a426
MOHAMED MEHIAOUI <mohamed.mehiaoui8@etu.univ-lorraine.fr>
2025-06-15 00:36:31 +0200

    ajout lien vers site iut

--- src/composants/boutonLien.jsx
+++ src/composants/boutonLien.jsx
@@ -4,7 +4,7 @@ import '../styles/boutonLien.css'
 
 function Boutonlien(){
     return(
-        <a class="toBUT" href='#'>Voir la formation</a>
+        <a class="toBUT" href='https://www.bing.com/ck/a?!&&p=ef9dea2e5cad42f6081024b605a2577b1684bc12607464c66df55b64a14359beJmltdHM9MTc0OTg1OTIwMA&ptn=3&ver=2&hsh=4&fclid=3e22731c-3f26-60a3-2711-65173e076144&psq=iut+saint+di%c3%a9&u=a1aHR0cHM6Ly9pdXRzZC51bml2LWxvcnJhaW5lLmZyLw&ntb=1' target='_blanck'>Voir la formation</a>
     );
 }
 
