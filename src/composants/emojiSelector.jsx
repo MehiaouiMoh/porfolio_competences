ccb9de2e26148ca02c962b205be6549c8dddb6d4
MOHAMED MEHIAOUI <mohamed.mehiaoui8@etu.univ-lorraine.fr>
2025-06-22 21:39:55 +0200

    responsive

--- src/composants/emojiSelector.jsx
+++ src/composants/emojiSelector.jsx
@@ -19,7 +19,7 @@ export default function EmojiSelector() {
       {emojis.map((emoji, index) => (
         <div
           key={index}
-          className={`w-16 h-16 rounded-[20%] flex justify-center items-center text-3xl cursor-pointer transition-transform duration-300
+          className={`w-16 h-13 rounded-[20%] flex justify-center items-center text-3xl cursor-pointer transition-transform duration-300
             ${selected === index ? "bg-cyan-400 rotate-3 scale-110" : "bg-gray-500"}
           `}
         >
